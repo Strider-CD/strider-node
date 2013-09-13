c9a5173f3b063e2d25b91032a70544ce2589ad1f
Jared Forsyth <jared@jaredforsyth.com>
2013-09-12 23:58:42 -0600

    updating for new api

--- worker.js
+++ worker.js
@@ -1,28 +1,33 @@
 var path = require('path')
 
 module.exports = {
-  // an object that defines the schema for configuration
-  config: null,
   // Initialize the plugin for a job
   //   config:     taken from DB config extended by flat file config
   //   job & repo: see strider-runner-core
   //   cb(err, initialized plugin)
-  init: function (config, job, repo, cb) {
-    cb(null, {
-      // string or list - to be added to the PATH
-      path: path.join(__dirname, '../node_modules/npm/bin'),
+  init: function (config, job, context, cb) {
+   var ret = {
       // any extra env variables
       env: {},
       // For each phase that you want to deal with, provide either a shell
       // string or fn(context, done)
       prepare: 'npm install',
-      test: 'npm test',
       cleanup: 'rm -rf node_modules'
-    });
+    }
+    if (config.test) {
+      ret.test = 'npm test'
+    }
+    if (config.runtime !== 'whatever') {
+      ret.env.N_PREFIX = path.join(context.dataDir, '.n')
+      // string or list - to be prefixed to the PATH
+      ret.path = [path.join(__dirname, 'node_modules/n/bin'), ret.env.N_PREFIX + '/bin']
+      ret.environment = 'n ' + config.runtime
+    }
+    cb(null, ret)
   },
   // if provided, autodetect is run if the project has *no* plugin
   // configuration at all.
-  gumshoe: {
+  autodetect: {
     filename: 'package.json',
     exists: true,
     language: 'node.js',
